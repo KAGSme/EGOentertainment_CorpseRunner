089205f95c2a91c579984b0415132fef86f9ef47
JimC <jimc1664@outlook.com>
2015-04-27 21:42:12 +0100

    Some things in life are bad They can really make you mad Other things just make you swear and curse. When you're chewing on life's gristle Don't grumble, give a whistle And this'll help things turn out for the best...
    And...always look on the bright side of life...
    Always look on the light side of life...

--- Corpse Runner Ultimate/CR Unity Game Project/Assets/Our Assets/Scripts/CharacterMotor.cs
+++ Corpse Runner Ultimate/CR Unity Game Project/Assets/Our Assets/Scripts/CharacterMotor.cs
@@ -6,16 +6,16 @@ public class CharacterMotor : MonoBehaviour {
     public string player;
     public float maxSpeedX = 10f;
     public float airSpeedX = 30f;
-	public float wallSlideSpeedForce = 3f;
+    public float wallSlideSpeedForce = 3f;
     public float jumpForce = 20f;
     public float wallJumpForce = 20f;
-	public float inverseGravity = 5;
-	float currentControlX;
+    public float inverseGravity = 5;
+    float currentControlX;
     bool _grounded;
     bool _walledR;
     bool _walledL;
     public bool canControlX = true;
-	float inputX;
+    float inputX;
     public Transform groundCheck;
     public Transform wallCheckR;
     public Transform wallCheckL;
@@ -24,97 +24,125 @@ public class CharacterMotor : MonoBehaviour {
     public LayerMask whatIsGround;
     public LayerMask whatIsClimbableWall;
 
-	// Use this for initialization
-	void Start () 
-    {
-        if (player == "2")
-        {
-            rigidbody.useGravity = false;
-            wallSlideSpeedForce = -wallSlideSpeedForce;
-            jumpForce = -jumpForce;
-            wallJumpForce = -wallJumpForce;
+    //!cache things is your friend
+    Rigidbody2D Body;  //todo - change addForces to just vector2's
+    Transform Trnsfrm;
+    PointListSource Mid;
+
+    void Awake() {
+        Body = rigidbody2D;
+        Trnsfrm = transform;
+
+        var go =  GameObject.Find("Mid");
+        if(go) {
+            Mid = go.GetComponent<Middle>().Src;
+            Mid.Trnsfrm = go.transform;
+        }
+    }
+
+    void Start() {
+        if(player == "2") {
+            //Body.useGravity = false;
+       //     wallSlideSpeedForce = -wallSlideSpeedForce;
+        //    jumpForce = -jumpForce;
+     //       wallJumpForce = -wallJumpForce;
         }
         _grounded = false;
         _walledR = false;
         _walledL = false;
         canControlX = true;
-	}
 
-    void Update()
-    {
-        rigidbody.velocity = new Vector3(Mathf.Clamp(rigidbody.velocity.x, -maxSpeedX, maxSpeedX), rigidbody.velocity.y, 0);
+        Rot = Body.rotation;
+    }
+
+    void Update() {   //?  why this not in fixed update?
+        Body.velocity = new Vector2(Mathf.Clamp(Body.velocity.x, -maxSpeedX, maxSpeedX), Body.velocity.y);
 
         Jumping();
     }
-	
-	void FixedUpdate () 
-    {
+
+    void FixedUpdate() {
         BasicMotorMovement();
         WallSlide();
-	}
+    }
+
+
+    //these are updated in BasicMotorMovement
+    Vector2 xAx = Vector2.right, yAx = Vector2.up;  //todo for player2 intialise these correct
+    float Rot;
 
     /// <summary>
     /// This controls the the basic horizontal movement of the character, 
     /// it uses the booleans _grounded to determine how movement should be dealt with
     /// </summary>
-    void BasicMotorMovement()
-    {
-        if (player == "2")
-        {
-            rigidbody.AddForce(new Vector3(0, inverseGravity, 0));
+    /// 
+    void BasicMotorMovement() {
+
+        var gravD = new Vector2(0, -1.0f);
+        if(player == "2") {  //! string checks are bad
+            gravD *= -1;
         }
+        if(Mid != null)
+            Mid.gravMod(ref gravD, Body.position );
+        Body.AddForce(gravD*9.8f);
 
+        Body.MoveRotation(Rot = Mathf.LerpAngle(Rot, Mathf.Atan2(gravD.x, -gravD.y) * Mathf.Rad2Deg, 4.0f * Time.deltaTime));
+
+        if(player == "2") {  //! string checks are bad
+       //     xAx = -xAx;
+        //    yAx = -yAx;
+        }
+        xAx = new Vector2(-gravD.y, gravD.x);
+        yAx = -gravD;
+
+
+        var ret = new Collider2D[1]; 
         // these booleans determine what the character is in contact with, it uses raycasts and layermasks to do this
-        _grounded = Physics.CheckSphere(groundCheck.position, groundRadius, whatIsGround);
-        _walledR = Physics.CheckSphere(wallCheckR.position, wallRadius, whatIsClimbableWall);
-        _walledL = Physics.CheckSphere(wallCheckL.position, wallRadius, whatIsClimbableWall);
+        _grounded = Physics2D.OverlapCircleNonAlloc(groundCheck.position, groundRadius, ret, whatIsGround) != 0;
+        _walledR = Physics2D.OverlapCircleNonAlloc(wallCheckR.position, wallRadius, ret, whatIsClimbableWall) != 0;
+        _walledL = Physics2D.OverlapCircleNonAlloc(wallCheckL.position, wallRadius, ret, whatIsClimbableWall) != 0;
 
-        inputX = Input.GetAxis("Horizontal"+player);
+        inputX = Input.GetAxis("Horizontal" + player);
         currentControlX = inputX * maxSpeedX;
 
-        if (canControlX && _grounded)
-        {
-            rigidbody.velocity = new Vector3(currentControlX, rigidbody.velocity.y, 0);
+        if(canControlX && _grounded) {
+
+
+            Body.velocity = currentControlX * xAx + Vector2.Dot(Body.velocity, yAx)*yAx;
         }
 
-        if (canControlX && !_grounded)
-        {
-            if (inputX < 0) rigidbody.AddForce(new Vector3(-10, 0, 0));
-            if (inputX > 0) rigidbody.AddForce(new Vector3(10, 0, 0));
+        if(canControlX && !_grounded) {
+            float flyForce = 10;
+            if(inputX < 0) Body.AddForce(xAx * -flyForce);
+            if(inputX > 0) Body.AddForce(xAx * flyForce);
         }
     }
 
     /// <summary>
     /// this causes the player character to slide down when in contact with the wall
+    ///! can modify gravity instead 
     /// </summary>
-    void WallSlide()
-    {
-        if (_walledR || _walledL)
-        {
-            rigidbody.AddForce(new Vector3(0, -wallSlideSpeedForce, 0));
+    void WallSlide() {
+        if(_walledR || _walledL) {
+            Body.AddForce( yAx * -wallSlideSpeedForce );
         }
     }
 
     /// <summary>
     /// This deals with the player character jumping as well as the input for it
     /// </summary>
-    void Jumping()
-    {
-        if (Input.GetButtonDown("Jump" + player))
-        {
-            if (_grounded)
-            {
-                rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+    void Jumping() {
+        if(Input.GetButtonDown("Jump" + player)) {
+            if(_grounded) {
+                Body.AddForce( yAx * jumpForce );
             }
 
-            if (_walledL)
-            {
-                rigidbody.AddForce(new Vector3(300, wallJumpForce, 0));
+            if(_walledL) {
+                Body.AddForce(xAx * 300 + yAx * wallJumpForce);
             }
 
-            if (_walledR)
-            {
-                rigidbody.AddForce(new Vector3(-300, wallJumpForce, 0));
+            if(_walledR) {
+                Body.AddForce(xAx*-300 + yAx * wallJumpForce);
             }
         }
     }
