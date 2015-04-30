af5d35665601d257e0a6be2abce7692f5cb26dc2
JimC <jimc1664@outlook.com>
2015-04-30 14:06:12 +0100

    Life's a piece of shit When you look at it Life's a laugh and death's a joke, it's true. You'll see it's all a show Keep 'em laughing as you go Just remember that the last laugh is on you.

--- Corpse Runner Ultimate/CR Unity Game Project/Assets/Our Assets/Scripts/CameraTrack.cs
+++ Corpse Runner Ultimate/CR Unity Game Project/Assets/Our Assets/Scripts/CameraTrack.cs
@@ -6,9 +6,9 @@ public class CameraTrack : CommonBehaviour {
 
     public CharacterMotor[] Targets;
     //public float MaxZoom = 16, MinZoom = 8, 
-    public float PlayerRadius = 6, PanSpeed = 0.1f, ZoomSpeed = 0.2f, RotSpeed = 2.0f;
+    public float PlayerRadius = 6, PanSpeed = 0.1f, ZoomSpeed = 0.2f, RotSmooth = 2.0f, RotSpeed = 5.0f;
 
-    Vector2 DesP; float DesZ, PosZ;
+    Vector2 DesP; float DesZ, PosZ, Rot, DesRot;
 
     Camera Cam;
  
@@ -22,16 +22,48 @@ public class CameraTrack : CommonBehaviour {
             return;
         }
 
+        DesRot =Rot = Trnsfrm.eulerAngles.z;
 	}
 	
 
 	void FixedUpdate () {
 
+
+        var up = (Targets[0].yAx - Targets[1].yAx) * 0.5f;
+
+        if(Vector2.Dot(Targets[0].yAx, Targets[1].yAx) <0.95f ) {
+            DesRot = Mathf.Atan2(-up.x, up.y) * Mathf.Rad2Deg;
+
+          //  Debug.Log("dot  " + Vector2.Dot(Targets[0].yAx, Targets[1].yAx));
+        } else {
+            var r1 =  Mathf.Atan2(-Targets[0].yAx.x, Targets[0].yAx.y) * Mathf.Rad2Deg;
+            var r2 =  Mathf.Atan2(Targets[1].yAx.x, -Targets[1].yAx.y) * Mathf.Rad2Deg;
+            var r3 = Mathf.Atan2(-Targets[0].yAx.y, -Targets[0].yAx.x) * Mathf.Rad2Deg;
+            var r4 = Mathf.Atan2(Targets[1].yAx.y, Targets[1].yAx.x) * Mathf.Rad2Deg;
+            
+            var d1 =  Mathf.Abs(Mathf.DeltaAngle(r1, DesRot)); 
+            var d2 =  Mathf.Abs(Mathf.DeltaAngle(r2, DesRot)); 
+            var d3 =  Mathf.Abs(Mathf.DeltaAngle(r3, DesRot)); 
+            var d4 =  Mathf.Abs(Mathf.DeltaAngle(r4, DesRot)); 
+
+            if( d1 > d3 ) { d1 = d3; r1 = r3; }
+            if( d2 > d4 ) { d2 = d4; r2 = r4; }
+            //  Debug.Log("dot  " + Vector2.Dot(Targets[0].yAx, Targets[1].yAx) + "  dr " + DesRot + "  r1 " + r1 + "  r2 " + r2 + "  f1 " + Mathf.DeltaAngle(r1, DesRot) + "  f2 " + Mathf.DeltaAngle(r2, DesRot));
+            
+            
+            DesRot = (d1<d2) ? r1 : r2;
+        }
+        var angs = Trnsfrm.localEulerAngles;
+        Rot = Mathf.MoveTowardsAngle(Rot, DesRot, Time.deltaTime * RotSpeed);
+
+        angs.z = Mathf.LerpAngle(angs.z, Rot, Time.deltaTime * RotSmooth);
+        Trnsfrm.localEulerAngles = angs;            
+
+
         Vector2 p1 = (Vector2)Targets[0].Trnsfrm.position, p2 = (Vector2)Targets[1].Trnsfrm.position;
 
-//        float mnX = Bounds.bounds.min.x, mxX = Bounds.bounds.max.x;   bounds for camera?
- //       float mnY = Bounds.bounds.min.y, mxY = Bounds.bounds.max.y;
-
+        p1 = Trnsfrm.InverseTransformPoint(p1);
+        p2 = Trnsfrm.InverseTransformPoint(p2);
 
         Vector2 trv = new Vector2(PlayerRadius, PlayerRadius);
 
@@ -97,23 +129,11 @@ public class CameraTrack : CommonBehaviour {
         */
         DesP = (bl + tr) * 0.5f;
 
+        DesP = Trnsfrm.TransformPoint(DesP);
+
         var p = Vector2.Lerp((Vector2)Trnsfrm.position, DesP, hHght * PanSpeed * Time.deltaTime);
         Trnsfrm.position = new Vector3(p.x, p.y, DesZ);
 
 
-
-       // Quaternion r1 = Quaternion.LookRotation(Vector3.forward, Targets[0].yAx), r1 = Quaternion.LookRotation(Vector3.forward, Targets[1].yAx);
-
-
-        var up = (Targets[0].yAx-Targets[1].yAx )*0.5f;
-
-        if(up.sqrMagnitude > 0.000001f) {
-
-            float ang = Mathf.Atan2(-up.x, up.y)*Mathf.Rad2Deg;
-            var angs = Trnsfrm.localEulerAngles;
-            angs.z = Mathf.LerpAngle(angs.z, ang, Time.deltaTime * RotSpeed );
-            Trnsfrm.localEulerAngles = angs;
-            
-        }
 	}
 }
